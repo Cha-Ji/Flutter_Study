bd3c4098e53f77c361716b55401ac4df1ae7211e
차지원 <chajiwon100785@gmail.com>
2020-11-12 18:29:02 +0900

    2주차 : 섹션2 [appbar, body, assetimage, app icon setting]

---
+++ flutter_app3/android/app/src/main/kotlin/com/chajichajitest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chajichajitest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_rich/android/app/src/main/kotlin/chaji/co/MainActivity.kt
@@ -0,0 +1,6 @@
+package chaji.co
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
